9e88291bc7c22480e0dd1d654b2d78a45ffd9dcf
Eugene Palagin <EvgeniyPalagin@gmail.com>
2019-12-05 20:37:59 +0300

    Добавлен HTTPEnum

---
+++ 03 - OOP/HTTPEnum.playground/Contents.swift
@@ -0,0 +1,12 @@
+enum HttpErrorCodes: Int {
+    case switchingProtocols = 101
+    case processing = 102
+    case ok = 200
+    case created = 201
+    case accepted = 202
+    case nonAuthoritativeInformation = 203
+    case noContent = 204
+    case resetContent = 205
+    case partialContent = 206
+    case multiStatus = 207
+}
